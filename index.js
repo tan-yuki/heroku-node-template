d1928cab6bf5d54cef857795977be07da088ec52
tan-yuki <a0673040@gmail.com>
2020-09-12 21:10:14 +0900

    Bugfix

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .get('/hello', (req, res) => 'Hello world!')
+  .get('/hello', (req, res) => res.send('Hello world!'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
